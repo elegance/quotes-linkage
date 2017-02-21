452d3f443ebb3ce64b5e2c4652fac98f500e707f
webdevelop <webdevelop@sohu.com>
2017-02-21 23:39:07 +0800

    vue methods 方式取行情

--- public/js/vue-position.js
+++ public/js/vue-position.js
@@ -9,32 +9,39 @@ requirejs.config({
 });
 
 require(['vue', 'axios', 'socket.io', 'simpleQuote'], function(Vue, axios, io, simpleQuote) {
-    var app = new Vue({
-        el: '#app',
-        data: {
-            datas: []
-        },
-        computed: {
-            price: function() {
-                // ?
-            }
-        }
-    });
 
     axios.get('/data/position.json')
         .then((resp) => {
             var data = resp.data.data;
-            app.datas = data;
+            // app.datas = data;
 
             // 生成订阅的完整代码数组 ：代码 + "." + 市场
             var subCodes = data.map(item => item.code + '.' + item.market);
 
             // 调用公共方法 订阅
             simpleQuote.subscribe('nsp-vue-position', subCodes, (newQuote) => {
-                console.log('有新的行情推送至客户端:', newQuote);
+                console.log('(非全部ready)有新的行情推送至客户端:', newQuote);
             }).then((gt) => { //此处的gt 可以用来 访问 上面 subCodes的任意一个股票的行情了
-                console.log(`所有订阅的股票：${subCodes}， 可以访问其价格了。`)
-                console.log(`如 ${subCodes[0]} : ${gt(subCodes[0])}`);
+                // console.log(`所有订阅的股票：${subCodes}， 可以访问其价格了。`)
+                // console.log(`如 ${subCodes[0]} : ${gt(subCodes[0])}`);
+                var app = new Vue({
+                    el: '#app',
+                    data: {
+                        datas: data
+                    },
+                    methods: {
+                        price: function(row) {
+                            return gt(row.code + '.' + row.market);
+                        },
+                        positionPrice: function(row) {
+                            return this.price(row) * row.position;
+                        }
+                    }
+                });
+
+                simpleQuote.nsps['nsp-vue-position'].onNewQuote = newQuote => {
+                    console.log('(已全部ready) 有新的行情推送至客户端:', newQuote);
+                };
             });
 
         });
